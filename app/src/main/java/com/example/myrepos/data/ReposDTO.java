6dde6380293409687032ab1ee90ae4808e27a92e
Anthony Lopez <tony.lopez.15@gmail.com>
2019-10-04 13:58:20 -0400

    Adding new variables to the Repos Class

--- app/src/main/java/com/example/myrepos/data/ReposDTO.java
+++ app/src/main/java/com/example/myrepos/data/ReposDTO.java
@@ -2,14 +2,32 @@ package com.example.myrepos.data;
 
 import com.google.gson.annotations.SerializedName;
 
-public class ReposDTO {
+import java.io.Serializable;
+
+public class ReposDTO implements Serializable {
     @SerializedName("id")
     private int mId;
+
     @SerializedName("name")
     private String mName;
+
     @SerializedName("description")
     private String mDescription;
 
+    @SerializedName("language")
+    private String mlanguage;
+
+    @SerializedName("updated_at")
+    private String mupdated;
+
+    @SerializedName("stargazers_count")
+    private String fav;
+
+    @SerializedName("forks")
+    private String mforks;
+
+    @SerializedName("html_url")
+    private String url;
 
     public String getmDescription() {
         return mDescription;
@@ -33,5 +51,45 @@ public class ReposDTO {
 
     public void setmName(String mName) {
         this.mName = mName;
+    }
+
+    public String getMlanguage() {
+        return mlanguage;
+    }
+
+    public void setMlanguage(String mlanguage) {
+        this.mlanguage = mlanguage;
+    }
+
+    public String getMupdated() {
+        return mupdated;
+    }
+
+    public void setMupdated(String mupdated) {
+        this.mupdated = mupdated;
+    }
+
+    public String getFav() {
+        return fav;
+    }
+
+    public void setFav(String fav) {
+        this.fav = fav;
+    }
+
+    public String getMforks() {
+        return mforks;
+    }
+
+    public void setMforks(String mforks) {
+        this.mforks = mforks;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
